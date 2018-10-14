ebe333376da8930a22b5d765d19809f3292ef754
salmotairy <s.musaad@gmail.com>
2018-10-14 11:32:18 +0300

    just begining

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
     console.log('javascript connected!');
+    // $('body').append('<header></header>');
+    // $('body header').append('<nav></nav>');
+    // $('body header nav').addClass("navigation");
+
+
+
+    // <header>
+    //     <button id="home">Home</button>
+    //     <button id="help"> Help</button>
+    // </header>
+    // <h1> Tic Tac Toe</h1>
+    // <h2> Improve your strategic</h2>
+    // $('body').append('<div>Mode Selection</div>');
+
 
 })
