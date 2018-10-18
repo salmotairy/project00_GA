56b87e69982fc35329a28a26940a11ca9ff9a519
salmotairy <s.musaad@gmail.com>
2018-10-18 09:47:59 +0300

    publish to get link

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  console.log("javascript connected!");
-
+  $(".start").click(function() {
+    $(".landing-page").hide();
+    $(".main").show();
+    $(".main").css("display", "flex");
+  });
   var placesLeft = 9;
   var xArray = [];
   var oArray = [];
@@ -9,12 +12,13 @@ $(document).ready(function() {
   var draws = 0;
   var opponent;
   var turn;
-
+  var places = [11, 12, 13, 21, 22, 23, 31, 32, 33];
+  var firstMove = [11, 13, 22, 31, 33];
+  var player1 = "Player1";
+  var player2 = "Player2";
   $(".options2").hide();
-
   $(".options1").on("change", opts);
   $(".options2").on("change", opts);
-
   function opts() {
     opponent = $(".opponent:checked").val();
     if (opponent === "AI") {
@@ -29,7 +33,6 @@ $(document).ready(function() {
       alert("call the developer!\n something went wrong.");
     }
   }
-
   function myTurn(turn) {
     if (turn === "first") {
       $(".player1").show();
@@ -41,81 +44,50 @@ $(document).ready(function() {
       alert("call the developer!\n something went wrong.");
     }
   }
-
-  //   var opponent = $(".opponent:checked").val();
-  //   console.log(opponent);
-
-  //   if (opponent === "AI") {
-  //     $(".options2").show();
-  //     var turn = $(".turn:checked").val();
-  //     console.log(turn);
-  //   } else if (opponent === "human") {
-  //     console.log("human");
-
-  //   } else{
-
-  //   }
-
-  //     if (turn === "first") {
-  //       $(".player1").show();
-  //       $(".player2").hide();
-
-  //       // $(".names").css({
-  //       //   justifyContent: "flex-start"
-  //       // });
-  //       // $("p.player1").css({
-  //       //   width: "100px",
-  //       //   height: "35px"
-  //       // });
-  //       // $("input.player1").css({
-  //       //   width: "130px",
-  //       //   height: "30px"
-  //       // });
-  //       // $("input.player1").css("width", "60px");
-  //       // } else {
-  //       // $(".player1").hide();
-  //       //     $(".names").css({
-  //       //       justifyContent: "flex-end"
-  //       //     });
-  //       //     $("p.player1").css({
-  //       //       width: "100px",
-  //       //       height: "35px"
-  //       //     });
-  //       //     $("input.player1").css({
-  //       //       width: "130px",
-  //       //       height: "30px"
-  //       //     });
-  //       //   }
-  //     } else {
-  //       $(".options2").hide();
-
-  //       $(".player1").hide();
-  //       $(".player2").show();
-
-  //       //   $(".names").css({
-  //       //     justifyContent: "space-around"
-  //       //   });
-  //       // $("p.player1").css("width", "auto");
-  //       // $("input.player1").css("width", "auto");
-  //     }
-  //   }
-  // });
-
+  $("#submit").on("click", whoPlay);
+  function whoPlay() {
+    if ($(".player1").is(":hidden")) {
+      player1 = "AI";
+      player2 = $("input.player2").val();
+    } else if ($(".player2").is(":hidden")) {
+      player1 = $("input.player1").val();
+      player2 = "AI";
+    } else if ($(".player1").is(":visible") && $(".player2").is(":visible")) {
+      player1 = $("input.player1").val();
+      player2 = $("input.player2").val();
+    } else {
+      alert("call the developer!\n something went wrong.");
+    }
+    $("#1").text(player1 + " wins");
+    $("#2").text(player2 + " wins");
+    play();
+  }
+  function play() {
+    $("#board td")
+      .empty()
+      .off();
+    xArray = [];
+    oArray = [];
+    placesLeft = 9;
+    if (player1 === "AI") {
+      aiTurn("X");
+    }
+    $("#board td").click(onClick);
+  }
   function onClick() {
     if ($(this).text() === "") {
       if (placesLeft % 2 == 1) {
         $(this).text("X");
         xArray.push(Number($(this).attr("id")));
         placesLeft--;
-        console.log(placesLeft);
-        result = isGameOver(xArray);
-        if (result !== "") {
+        // result = isGameOver(xArray);
+        if (isGameOver(xArray) !== "") {
           if (xArray.length >= 3) {
             window.setTimeout(function() {
               xWins++;
               swal({
                 title: "Good job!",
-                text: "The first " + result,
+                text: player1 + " wins the game.",
                 icon: "images/thumps-up.png"
               });
               $("td#4").text(xWins);
@@ -131,6 +103,7 @@ $(document).ready(function() {
               icon: "success"
             });
             $("td#6").text(draws);
+            play();
           }, 200);
         }
       } else {
@@ -139,14 +112,14 @@ $(document).ready(function() {
         placesLeft--;
         console.log(placesLeft);
         if (oArray.length >= 3) {
-          var result = isGameOver(oArray);
-          if (result !== "") {
+          // var result = isGameOver(oArray);
+          if (isGameOver(oArray) !== "") {
             window.setTimeout(function() {
               // alert("The second " + result);
               swal({
                 title: "Good job!",
-                text: "The second " + result,
-                icon: "success"
+                text: player2 + " wins the game.",
+                icon: "images/thumps2.jpg"
               });
               oWins++;
               $("td#5").text(oWins);
@@ -173,53 +146,37 @@ $(document).ready(function() {
       }, 200);
     }
   }
-  function play() {
-    $("#board td")
-      .empty()
-      .off();
-    // var play = true;
-    xArray = [];
-    // console.log(xArray, oArray);
-    oArray = [];
-    result = "";
-    placesLeft = 9;
-    $("#board td").click(onClick);
-  }
-
-  play();
-  // function checkThree(num1,num2,num3,array)
-  // {
-  //   if (array.includes(num1) && array.includes(num2) && array.includes(num3)) {
-  //     return true
-  // }else {
-  //     return false
-
+  // function aiTurn(letter) {
+  //   if (placesLeft === 9) {
+  //     var id = "#" + firstMove[Math.floor(Math.random() * 5)].toString();
+  //     $(id).text(letter);
+  //   } else if (placesLeft === 8) {
+  //     if ($("#22").text() === "") {
+  //       $("#22").text(letter);
+  //     } else {
+  //       firstMove.splice(2, 1);
+  //       var id = "#" + firstMove[Math.floor(Math.random() * 4)].toString();
+  //       $(id).text(letter);
+  //     }
+  //   } else if (placesLeft === 7) {
+  //   }
   // }
+  play();
+  function checkThree(num1, num2, num3, array) {
+    return array.includes(num1) && array.includes(num2) && array.includes(num3);
+  }
   function isGameOver(array) {
-    if (array.includes(11) && array.includes(12) && array.includes(13)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(21) && array.includes(22) && array.includes(23)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(31) && array.includes(32) && array.includes(33)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(11) && array.includes(21) && array.includes(31)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(12) && array.includes(22) && array.includes(32)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(13) && array.includes(23) && array.includes(33)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(11) && array.includes(22) && array.includes(33)) {
-      console.log("won");
-      return "player won the game";
-    } else if (array.includes(13) && array.includes(22) && array.includes(31)) {
-      console.log("won");
-      return "player won the game";
+    if (
+      checkThree(11, 12, 13, array) ||
+      checkThree(21, 22, 23, array) ||
+      checkThree(31, 32, 33, array) ||
+      checkThree(11, 21, 31, array) ||
+      checkThree(12, 22, 32, array) ||
+      checkThree(13, 23, 33, array) ||
+      checkThree(11, 22, 33, array) ||
+      checkThree(13, 22, 31, array)
+    ) {
+      return " wins the game";
     }
     return "";
   }
